a4e810d3b76d2ec6a67470f8359b92235a3a8564
Jack Holy <jholy@carroll.edu>
2024-03-20 22:44:37 -0600

    Allow user to switch between chords properly

--- main.go
+++ main.go
@@ -1,43 +1,58 @@
 package main
 
 import (
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
 	// Create a new app and window
 	chordFinder := app.New()
-	myWindow := chordFinder.NewWindow("Ukulele Chord Diagrams")
+	window := chordFinder.NewWindow("Ukulele Chord Diagrams")
+
+	// Reset the size of the window
+	window.Resize(fyne.NewSize(800, 400))
 
 	// Define the chords
 	chords := []string{"C", "G", "F", "D", "Am", "A", "Dm", "G7", "Em", "E7", "A7", "C7"}
+
+	// Load the default chord diagram image
+	defaultChordImage := loadChordImage(chords[0])
+	defaultChordImage.FillMode = canvas.ImageFillContain
+	defaultChordImage.SetMinSize(fyne.NewSize(400, 300))
+
+	// Create a container for the image
+	imageContainer := container.New(layout.NewCenterLayout(), defaultChordImage)
 
 	// Create a dropdown widget for chords
 	chordDropdown := widget.NewSelect(chords, func(selectedChord string) {
 		// Load and display the corresponding chord diagram image
 		chordImage := loadChordImage(selectedChord)
-		myWindow.SetContent(container.NewVBox(
-			chordImage,
-		))
+		chordImage.FillMode = canvas.ImageFillContain
+		chordImage.SetMinSize(fyne.NewSize(400, 300)) // Adjust size as needed
+		imageContainer.Objects = []fyne.CanvasObject{chordImage}
+		imageContainer.Refresh()
 	})
 
 	// Set the default chord selection
 	chordDropdown.SetSelected(chords[0])
 
-	// Load the default chord diagram image
-	defaultChordImage := loadChordImage(chords[0])
+	// Create a container for the dropdown
+	dropdownContainer := container.New(layout.NewVBoxLayout(), chordDropdown)
+	dropdownContainer.Resize(fyne.NewSize(200, 400)) // Set size as needed
+
+	// Create a main container with horizontal layout
+	mainContainer := container.New(layout.NewHBoxLayout(), dropdownContainer, imageContainer)
 
 	// Set the initial content of the window
-	myWindow.SetContent(container.NewVBox(
-		chordDropdown,
-		defaultChordImage,
-	))
+	window.SetContent(mainContainer)
 
 	// Show the window
-	myWindow.ShowAndRun()
+	window.ShowAndRun()
 }
 
 // Load chord diagram image based on the selected chord
